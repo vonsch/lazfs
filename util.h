9db744c43a6ec9471b18f1bbf7985012a14e035e
Adam Tkac <adam.tkac@geodis.cz>
2013-05-15 11:09:55 +0200

    Add LOCK and UNLOCK helper macros
    Signed-off-by: Adam Tkac <adam.tkac@geodis.cz>

--- util.h
+++ util.h
@@ -8,6 +8,21 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
 #include <string.h>
+#include <pthread.h>
+
+#define LOCK(mutex) \
+    do { \
+	if (pthread_mutex_lock(&mutex) != 0) { \
+	    abort(); /* Failing to acquire lock means a bug */ \
+	} \
+    } while (0)
+
+#define UNLOCK(mutex) \
+    do { \
+	if (pthread_mutex_unlock(&mutex) != 0) { \
+	    abort(); /* Failing to release a lock means a bug */ \
+	} \
+    } while (0)
 
 /*
  * Returns non-zero value if fpath should be handled specially (i.e.
